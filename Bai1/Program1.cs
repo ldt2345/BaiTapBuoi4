c868718096ab507a096ef10ccff2e2833f375b1f
A209PC49\Administrator <leductu23456789@gmail.com>
2023-09-06 16:13:58 +0700

    bai1buoi4

---
+++ Bai1/Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            XeNoiThanh xnt = new XeNoiThanh();
+            XeNgoaiThanh xngt = new XeNgoaiThanh();
+            xnt.Xuat();
+            xngt.XuatNgoaiThanh();
+            Console.ReadKey();
+        }
+    }
+}
