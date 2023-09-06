eb826df03539eb32b6b5961cf8cb02a77d44fcce
A209PC49\Administrator <leductu23456789@gmail.com>
2023-09-06 15:23:14 +0700

    bai1buoi4

---
+++ Bai1/XeNoiThanh.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    class XeNoiThanh
+    {
+        string maSoChuyen;
+
+        public string MaSoChuyen
+        {
+            get { return maSoChuyen; }
+            set { maSoChuyen = value; }
+        }
+        string hoTenTaiXe;
+
+        public string HoTenTaiXe
+        {
+            get { return hoTenTaiXe; }
+            set { hoTenTaiXe = value; }
+        }
+        int soXe;
+
+        public int SoXe
+        {
+            get { return soXe; }
+            set { soXe = value; }
+        }
+        int soTuyen;
+
+        public int SoTuyen
+        {
+            get { return soTuyen; }
+            set { soTuyen = value; }
+        }
+        double soKM;
+
+        public double SoKM
+        {
+            get { return soKM; }
+            set { soKM = value; }
+        }
+        double doanhThu;
+
+        public double DoanhThu
+        {
+            get { return doanhThu; }
+            set { doanhThu = value; }
+        }
+        public XeNoiThanh(string maSoChuyen, string hoTenTaiXe, int soXe, int soTuyen, double soKM, double doanhThu)
+        {
+            this.maSoChuyen = maSoChuyen;
+            this.hoTenTaiXe = hoTenTaiXe;
+            this.soKM = soKM;
+            this.soXe = soXe;
+            this.soTuyen = soTuyen;
+            this.doanhThu = doanhThu;
+        }
+        public double TinhTongDoanhThu()
+        {
+
+        }
+    }
+}
